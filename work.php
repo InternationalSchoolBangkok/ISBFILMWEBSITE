61b1e933d3d506972246b0747557b99d34e58554
hatterkiller <michaelshinoda287@gmail.com>
2016-02-23 10:38:00 +0700

    staff

--- work.php
+++ work.php
@@ -7,9 +7,15 @@
     
         <div class="dropdown">
             <h1 style="display:inline-block">Genres: </h1>
-            <span style="display:inline-block">Genre Selector</span>
+            
             <div class="dropdown-content">
-                <p>Hello World!</p>
+                <ul>
+                    <li class="genreselect"><a href="#action">Action</a></li>
+                    <li class="genreselect"><a href="#Horror">Horror</a></li>
+                    <li class="genreselect"><a href="#scifi">Sci-Fi</a></li>
+                    <li class="genreselect"><a href="#drama">Drama</a></li>
+                    <li class="genreselect"><a href="#fiction">Fiction</a></li>
+                </ul>
             </div>
         </div>
     
@@ -17,17 +23,45 @@
     </div>
     
     
+    <div class="genre">
+        <table>
+            <tr id="action">
+                <td><img src="12696230_10204562579344126_562056904_n.jpg"></td>
+                <td>
+                    <div class="text">
+                        <h1>Action</h1>
+                        Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat.
+                    </div>
+                </td>
+            </tr>
+            
+            <tr id="Horror">
+                <td><img src="12746464_10204562578984117_1338917545_n.jpg"></td>
+                <td>
+                    <div class="text">
+                        <h1>Horror</h1>
+                        Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat.
+                    </div>
+                </td>
+            </tr>
+            
+        
+        </table>
     
+    
+    </div>
 </div>
 <style>
     .dropdown {
     position: relative;
     display: inline-block;
+        text-align: center;
+        width:100%;
+        padding-top: 30px;
 }
 
 .dropdown-content {
     display: none;
-    position: absolute;
     background-color: #f9f9f9;
     min-width: 160px;
     box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
@@ -37,7 +71,28 @@
 
 .dropdown:hover .dropdown-content {
     display: block;
+    line-height: 20px;
 }
+    .genreselect{
+        text-align: center;
+        display: block;
+    
+    }
+    .genreselect a{
+        color:black;
+    }
+    
+    .genre{
+        width: 75%;
+        margin: 0 auto;
+        margin-top: 40vh;
+        
+    }
+    .genre img{
+        width:20vw;
+    }
+    
+
     
     .box{
         color:red;
@@ -56,7 +111,7 @@
         position: absolute;
         text-align: center;
         color:black;
-        top:0;
+        top:30;
         right:0;
         left:0;
         font-size:5vh;
@@ -76,6 +131,9 @@
         margin: 0;
         color:white;
         font-family: Helvetica;
+    }
+    a{
+        text-decoration: none;
     }
 
 </style>
